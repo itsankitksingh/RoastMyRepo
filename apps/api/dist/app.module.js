cf196d25e50eb5e55998d713eeb67a674c3683ab
Ankit <itsankitksingh@gmail.com>
2026-03-19 21:50:55 +0530

    start fix

--- apps/api/dist/app.module.js
+++ apps/api/dist/app.module.js
@@ -12,12 +12,19 @@ const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const roast_controller_1 = require("./roast.controller");
 const roast_service_1 = require("./roast.service");
+const serve_static_1 = require("@nestjs/serve-static");
+const path_1 = require("path");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            serve_static_1.ServeStaticModule.forRoot({
+                rootPath: (0, path_1.join)(__dirname, '..', '..', '..', 'apps', 'web', 'dist', 'web', 'browser'),
+                exclude: ['/api*'],
+            }),
+        ],
         controllers: [app_controller_1.AppController, roast_controller_1.RoastController],
         providers: [app_service_1.AppService, roast_service_1.RoastService],
     })
